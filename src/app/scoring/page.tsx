ef0fc0217825fe4c2d39ba78864534ff5d0a3921
agustriad <agusmemons90@gmail.com>
2025-04-24 19:44:49 +0700

    Revert "reset data"
    This reverts commit 36d77ea0e1bfd7cc56baca95f23e8b0f2e69bada.

--- src/app/scoring/page.tsx
+++ src/app/scoring/page.tsx
@@ -6,7 +6,7 @@ import LoadingIcon from '@/components/LoadingIcon';
 import { useState } from 'react';
 
 export default function Page() {
-  const { result, resetResult } = useScoringStore((state) => state);
+  const result = useScoringStore((state) => state.result);
   const TOTAL_STUDENT = result.length;
   const TOTAL_ASPECT = result[0].length;
   const [loading, setLoading] = useState(false);
@@ -25,7 +25,6 @@ export default function Page() {
     await new Promise((res) => setTimeout(res, 500)); // Simulasi delay
     console.log(JSON.stringify(hasil, null, 2));
     alert('Cek console!');
-    resetResult();
     setLoading(false);
   };
 
